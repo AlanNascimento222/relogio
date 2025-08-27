f916e9551af1a25122a403ca57e33673c159f7f4
Alan222 <alanascimento222@hotmail.com>
2025-08-27 11:54:01 -0300

    Add files via upload
    updates no Designe e implementado responsividade.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 function carregarRelogio() {
-
+    const semanaDisplay = document.getElementById('semana')
+    const horaDisplay = document.getElementById('hora')
+    const minutoDisplay = document.getElementById('minuto')
+    const segundoDisplay = document.getElementById('segundo')
+    const diasSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-feira", "Quinta-Feira", "Sexta-Feira", "Sabado"]
     const now = new Date
 
+    let semana = diasSemana[now.getDay()]
     let hora = now.getHours()
     let minutos = now.getMinutes()
     let segundos = now.getSeconds()
     const tela = document.getElementById('display')
 
+
+
     hora = hora < 10 ? '0' + hora : hora
     minutos = minutos < 10 ? '0' + minutos : minutos
     segundos = segundos < 10 ? '0' + segundos : segundos
 
-    tela.textContent = `${hora}:${minutos}:${segundos}`
+    horaDisplay.textContent = hora
+    minutoDisplay.textContent = minutos
+    segundoDisplay.textContent = segundos
+    semanaDisplay.textContent = semana
 
 }
 
